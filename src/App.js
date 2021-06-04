37b53f5844609f2d824384708364860bfb527622
Mikael Aurell <aurell.mikael@gmail.com>
2021-06-04 11:20:42 +0200

    added css, header, body and footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Vonage Stream
       </header>
+        <body className="App-body">
+        Hello Word
+        </body>
+
+        <footer className="App-footer">&copy; Mikael Aurell</footer>
     </div>
   );
 }
